1810f639d9cb99264e2664983371d339af714df6
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-09 19:54:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,7 @@ var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 function addElementToEndOfArray (array, element) {
   return [element,...array]
 }
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element)
+  return array
+}
